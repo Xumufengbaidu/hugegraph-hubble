ca6b122a8f6630855eafab58743543c32cd610ba
Xumufengbaidu <1119034261@qq.com>
2022-05-16 15:10:23 +0800

    k8stoken width

--- hubble-fe/src/components/k8sToken/index.tsx
+++ hubble-fe/src/components/k8sToken/index.tsx
@@ -5,16 +5,15 @@ function Index() {
   const [data, setData] = useState({ token: '' });
   useEffect(() => {
     api.getK8sToken().then((res) => {
-      if (res.status === "200") {
+      if (res.status === 200) {
         setData(res.data);
       }
     });
   }, []);
   return (
-    <div className="query_list_container graphData_wrapper">
-      <div className="topDiv"></div>
+    <div className="query_list_container">
       <div style={{ fontSize: '22px' }}>K8s Token:</div>
-      <div style={{ fontSize: '16px' }}>{data.token}</div>
+      <div style={{ fontSize: '16px',backgroundColor:'white',width:"100%",wordWrap: "break-word"}}>{data.token}</div>
     </div>
   );
 }
